b9926ac050cfa425b3e740dc3ed231de836197ca
JacobChartier <55593549+JacobChartier@users.noreply.github.com>
2024-04-12 10:33:23 -0400

    Inventory saving

--- Assets/Scripts/Inventory/InvFile.cs
+++ Assets/Scripts/Inventory/InvFile.cs
@@ -1,31 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using UnityEditor.ShaderGraph.Internal;
 
 public static class InvFile
-{ 
+{
     public static void Save(string path, Inventory inventory)
     {
-        using (StreamWriter sw = new StreamWriter(path))
-        {
-            // Header
-            sw.WriteLine($"# This file has been automatically generated on {DateTime.Today:yyyy-MM-dd} at {DateTime.Now:HH:mm:ss}.\n# It contains inventory data for entity \"{inventory.id}\".\n");
+        using StreamWriter sw = new(path);
 
-            // Saved Data
-            sw.WriteLine($"INVENTORY_ID: \"{inventory.id}\"");
-            foreach (var item in inventory.items)
-            {
-                sw.WriteLine($"\t{item.ToString()}");
-            }
+        // Header
+        sw.WriteLine($"# This file has been generated on {DateTime.Today:yyyy-MM-dd} at {DateTime.Now:HH:mm:ss}.\n# It contains inventory data for entity \"{inventory.id}\".\n");
 
-            // Footer
-            sw.WriteLine($"");
-        }
+        // Data
+        sw.WriteLine($"INVENTORY_ID: \"{inventory.id}\"");
+
+        foreach (var item in inventory.items)
+            sw.WriteLine($"{SerializeItem(item, inventory)}");
+
+        // Footer
+        sw.WriteLine($"\n# {inventory.items.Count.ToString()} items saved for entity \"{inventory.id}\".");
     }
 
-    public static List<RetrievedItems> Load(string path, Inventory inventory)
+    private static string SerializeItem(Item item, Inventory inventory)
     {
-        List<RetrievedItems> items = new List<RetrievedItems>();
+        string output = "\t";
+
+        output += $"ITEM: {item.GetType().ToString()} ";
+
+        output += "{ ";
+
+        if (item.GetSlotID() > -1)
+            output += $"SlotID: {item.GetSlotID().ToString()}, ";
+
+        if (item.RemainingUses != 5)
+            output += $"RemainingUses: {item.RemainingUses.ToString()}, ";
+
+        output = output.TrimEnd(',', ' ');
+
+        output += " }";
+
+        return output;
+    }
+
+    private static Item DeserializeItem(string line)
+    {
+        return default;
+    }
+
+    public static List<DeserializedItem> Load(string path, Inventory inventory)
+    {
+        List<DeserializedItem> items = new();
         var lines = File.ReadAllLines(path);
 
         for (int i = 0; i < lines.Length; i++)
@@ -34,17 +61,17 @@ public static class InvFile
             int slot = -2;
             System.Type type = default;
 
-            RetrievedItems retrievedItems = new RetrievedItems();
+            DeserializedItem retrievedItems = new DeserializedItem();
 
-            //if (lines[i].ToString().Contains("TYPE"))
-            //{
-            //    s_type = ReadData<string>("TYPE", lines[i]);
-            //    retrievedItems.type = System.Type.GetType(s_type);
+            if (lines[i].ToString().Contains("TYPE"))
+            {
+                s_type = ReadData<string>("TYPE", lines[i]);
+                retrievedItems.type = System.Type.GetType(s_type);
 
 
-            //    retrievedItems.slotID = ReadData<int>("SLOT_ID", lines[i + 1]);
-            //    Debug.Log($"<color=#00FF00>{retrievedItems.type}</color> {{{retrievedItems.slotID}}}");
-            //}
+                retrievedItems.slotID = ReadData<int>("SLOT_ID", lines[i + 1]);
+                UnityEngine.Debug.Log($"<color=#00FF00>{retrievedItems.type}</color> {{{retrievedItems.slotID}}}");
+            }
 
             if (type != null)
             {
@@ -69,9 +96,9 @@ public static class InvFile
 }
 
 [Serializable]
-public class RetrievedItems
+public struct DeserializedItem
 {
     public System.Type type;
     public int slotID;
-    public int quantity;
+    public int RemainingUses;
 }
